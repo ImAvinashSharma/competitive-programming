0ad1ce71bf08c092cab5665868f468fa871504a7
Avinash Sharma <58262578+avi9360@users.noreply.github.com>
2020-08-18 19:06:21 +0530

    Create MaximumInTable.cpp

---
+++ MaximumInTable.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ui unsigned int
+#define lli long long int
+#define ll long long
+#define si(x)   scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x)  cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+int solve(int r, int c) {
+	if (r == 1 || c == 1)
+		return 1;
+	return solve(r - 1, c) + solve(r, c - 1);
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	//code hear
+	int n; cin >> n;
+	cout << solve(n, n) << endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
